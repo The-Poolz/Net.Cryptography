d6658cbf635625d0301e22d4f856df2f08353702
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-03-18 12:09:17 +0300

    Update target framework to `netstandard2.1`

--- src/Net.Cryptography.SHA256/SHA256.cs
+++ src/Net.Cryptography.SHA256/SHA256.cs
@@ -1,31 +1,32 @@
 ﻿using System.Text;
 
-namespace Net.Cryptography.SHA256;
-
-/// <summary>
-/// Provides a method for computing SHA256 hash of a string.
-/// </summary>
-public static class SHA256
+namespace Net.Cryptography.SHA256
 {
     /// <summary>
-    /// Computes the SHA256 hash of the specified string.
+    /// Provides a method for computing SHA256 hash of a string.
     /// </summary>
-    /// <param name="str">The input string to compute the hash for.</param>
-    /// <remarks>
-    /// This method computes the SHA256 hash for the given string using <see cref="System.Security.Cryptography.SHA256"/>.
-    /// The hash is computed on the UTF-8 representation of the input string.
-    /// </remarks>
-    /// <returns>A hexadecimal string representing the SHA256 hash of the input string.</returns>
-    public static string ToSha256(this string str)
+    public static class SHA256
     {
-        using var sha256Hash = System.Security.Cryptography.SHA256.Create();
-        var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(str));
-
-        var builder = new StringBuilder();
-        foreach (var t in bytes)
+        /// <summary>
+        /// Computes the SHA256 hash of the specified string.
+        /// </summary>
+        /// <param name="str">The input string to compute the hash for.</param>
+        /// <remarks>
+        /// This method computes the SHA256 hash for the given string using <see cref="System.Security.Cryptography.SHA256"/>.
+        /// The hash is computed on the UTF-8 representation of the input string.
+        /// </remarks>
+        /// <returns>A hexadecimal string representing the SHA256 hash of the input string.</returns>
+        public static string ToSha256(this string str)
         {
-            builder.Append(t.ToString("x2"));
+            using var sha256Hash = System.Security.Cryptography.SHA256.Create();
+            var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(str));
+
+            var builder = new StringBuilder();
+            foreach (var t in bytes)
+            {
+                builder.Append(t.ToString("x2"));
+            }
+            return builder.ToString();
         }
-        return builder.ToString();
     }
 }
